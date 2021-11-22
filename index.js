18a09b0f810af7c13ce9a8356ef621513680b969
yasminreisk <yasmin.reiss16@gmail.com>
2021-11-22 19:14:52 -0300

    Organizando o MySQL

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const customExpress = require ('./config/customExpress')
+const conexao = require ('./infra/conexao')
+const Tabelas = require ('./infra/tabelas')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if (erro) {
+        console.log (erro)
+    } else {
+        console.log ('conectado com sucesso')
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        Tabelas.init(conexao)
+
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
+
